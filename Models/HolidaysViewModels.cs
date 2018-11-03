ee8014e4d8452170fbfbcb209187e95131f5146a
bitsmithF1 <bitsmithF1@gmail.com>
2018-11-02 20:05:44 -0400

    Made HolidaysIndexViewModel extend ViewModelBase.

--- Models/HolidaysViewModels.cs
+++ Models/HolidaysViewModels.cs
@@ -5,7 +5,7 @@ using dotnet_webapp.Entities;
 
 namespace dotnet_webapp.Models
 {
-    public class HolidaysIndexViewModel
+    public class HolidaysIndexViewModel : ViewModelBase
     {
         public int Year { get; set; }
         public IEnumerable<Holiday> Holidays { get; set; }
